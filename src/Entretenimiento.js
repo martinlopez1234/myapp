c0445679ac47ff4d204b8c29ff167b599ef9f12e
martinlopez1234 <martin.lr@hotmail.com>
2023-02-07 17:53:11 +0000

    cambios

--- src/Entretenimiento.js
+++ src/Entretenimiento.js
@@ -32,10 +32,10 @@ function Entretenimiento() {
           <div class="card">
             <img src="https://images.unsplash.com/photo-1619869300502-ac8d409e7635?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" width={"370px"} height={"370px"} class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Tutoriales de computador</h5>
+              <h5 class="card-title">Memorize</h5>
               <p class="card-text">
-                Haciendo click en el boton azul debajo te mandara directamente a tutoriales de computadora.</p>
-              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.emol.com/">Ingresar a tutoriales computador</a></button>
+                Haciendo click en el boton azul debajo te mandara directamente a juego Memorize.</p>
+              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.emol.com/">Ingresar a Memorize</a></button>
             </div>
           </div>
         </div>
@@ -43,10 +43,10 @@ function Entretenimiento() {
           <div class="card ">
             <img src="https://images.unsplash.com/photo-1640537702474-3e503c21eefc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" width={"370px"} height={"370px"} class="card-img-top " alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Tutoriales de telefono</h5>
+              <h5 class="card-title">Sudoku</h5>
               <p class="card-text">
-              Haciendo click en el boton azul debajo te mandara directamente a tutoriales de telefono.</p>
-              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.lun.com/">Ingresar a tutoriales de telefono</a></button>
+              Haciendo click en el boton azul debajo te mandara directamente a juego Sudoku.</p>
+              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.lun.com/">Ingresar a Sudoku</a></button>
             </div>
           </div>
         </div>
@@ -54,10 +54,10 @@ function Entretenimiento() {
           <div class="card">
             <img src="https://images.unsplash.com/photo-1585338107529-13afc5f02586?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" width={"370px"} height={"370px"} class="card-img-top " alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Tutoriales de tablet</h5>
+              <h5 class="card-title">Crucigrama</h5>
               <p class="card-text">
-              Haciendo click en el boton azul debajo te mandara directamente a tutoriales de tablet.</p>
-              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.latercera.com/">Ingresar a tutoriales de tablet</a></button>
+              Haciendo click en el boton azul debajo te mandara directamente a juego Crucigrama.</p>
+              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.latercera.com/">Ingresar a Crucigrama</a></button>
             </div>
           </div>
         </div>
@@ -65,10 +65,10 @@ function Entretenimiento() {
           <div class="card">
             <img src="https://images.unsplash.com/photo-1594692707496-a9dc0498e175?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" width={"370px"} height={"370px"} class="card-img-top " alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Tutoriales de correo</h5>
+              <h5 class="card-title">Solitario</h5>
               <p class="card-text">
-              Haciendo click en el boton azul debajo te mandara directamente a tutoriales de correo.</p>
-              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.tvn.cl/">Ingresar a tutoriales de correo</a></button>
+              Haciendo click en el boton azul debajo te mandara directamente a juego Solitario.</p>
+              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.tvn.cl/">Ingresar a Solitario</a></button>
             </div>
           </div>
         </div>
@@ -76,10 +76,10 @@ function Entretenimiento() {
           <div class="card">
             <img src="https://images.unsplash.com/photo-1494059980473-813e73ee784b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" width={"370px"} height={"370px"} class="card-img-top " alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Tutoriales de whassap</h5>
+              <h5 class="card-title">Puzzle</h5>
               <p class="card-text">
-              Haciendo click en el boton azul debajo te mandara directamente a tutoriales de Whasapp.</p>
-              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.mega.cl/">Ingresar a tutoriales de whasap</a></button>
+              Haciendo click en el boton azul debajo te mandara directamente a juego Puzzle.</p>
+              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.mega.cl/">Ingresar a Puzzle</a></button>
             </div>
           </div>
         </div>
@@ -87,10 +87,10 @@ function Entretenimiento() {
           <div class="card">
             <img src="https://images.unsplash.com/photo-1533226458520-6f71cffeaa6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" width={"370px"} height={"370px"} class="card-img-top " alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Tutoriales de internet</h5>
+              <h5 class="card-title">Sopa de Letras</h5>
               <p class="card-text">
-              Haciendo click en el boton azul debajo te mandara directamente a tutoriales de navegacion en internet.</p>
-              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.t13.cl/">Ingresar tutoriales de internet</a></button>
+              Haciendo click en el boton azul debajo te mandara directamente a juego Sopa de Letras.</p>
+              <button className='btn btn-primary'><a className="text-white nav-link" href="https://www.t13.cl/">Ingresar Sopa de Letras </a></button>
             </div>
           </div>
         </div>
